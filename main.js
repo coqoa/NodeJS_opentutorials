d7ab4ed4834a256e6f14a9f56e806b435078e9b7
coqoa <choibm0208@gmail.com>
2021-11-09 17:00:31 +0900

    211109 Node.js-19.1.App 제작-Not found 구현

--- main.js
+++ main.js
@@ -7,49 +7,46 @@ let app = http.createServer(function(request,response){
     let _url = request.url;
     // console.log(_url); // queryString을 알아낼 수 있다.
     let queryData = url.parse(_url, true).query;
+    let pathname = url.parse(_url, true).pathname;
     // console.log(queryData);// http://localhost:3000/?id=HTML 입력시  {id : 'HTML'} 출력 
     //                           -> queryData의 변수에 담긴 값들은 객체.
     // console.log(queryData.id);// HTML 출력
     let title = queryData.id;
-    if(_url == '/'){
-        title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', function(err, description){
+    
 
-        let template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-        
-        `
-        // console.log(__dirname + url);
-        
-        // response.end(fs.readFileSync(__dirname + _url));
-        // response.end('egoing : '+url); //egoing : /3.html을 화면에 출력함
-        
-        response.end(template); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
-    });
-        
+    if(pathname==-'/'){
+        fs.readFile(`data/${title}`, 'utf8', function(err, description){
+            let template = `
+            <!doctype html>
+            <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+            </html>
+            `
+            // console.log(__dirname + url);
+            
+            // response.end(fs.readFileSync(__dirname + _url));
+            // response.end('egoing : '+url); //egoing : /3.html을 화면에 출력함
+            response.writeHead(200); //200은 파일을 성공적으로 전송했다
+            response.end(template); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
+        });
+    }else{
+        response.writeHead(404); //파일을 찾을 수 없으면 404
+        response.end('Not found');
+    }
+    
 });
 app.listen(3000);  
 
