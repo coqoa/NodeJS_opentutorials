ec2b44524193698b0fa872d1b1e0002bf0a32fa8
coqoa <choibm0208@gmail.com>
2021-11-09 18:03:51 +0900

    211109 Node.js-24.App 제작-글목록 출력하기

--- main.js
+++ main.js
@@ -15,8 +15,19 @@ let app = http.createServer(function(request,response){
     
     if(pathname == '/'){
         if(queryData.id == undefined){
+
+            fs.readdir('./data', function(error, filelist){
+                // 배열의 형태로 출력
+
                 let title = 'Welcome'
                 let description = 'Hello, Node.js'
+                
+                let list = '<ul>';
+                for(i=0; i<filelist.length; i++){
+                    list = list+`<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                }
+                list = list+'</ul>'
+
                 let template = `
                 <!doctype html>
                 <html>
@@ -26,11 +37,8 @@ let app = http.createServer(function(request,response){
                     </head>
                     <body>
                         <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            <li><a href="/?id=HTML">HTML</a></li>
-                            <li><a href="/?id=CSS">CSS</a></li>
-                            <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
+                        ${list}
+                        
                         <h2>${title}</h2>
                         <p>${description}</p>
                     </body>
@@ -38,31 +46,41 @@ let app = http.createServer(function(request,response){
                 `
                 response.writeHead(200); //파일을 성공적으로 전송하면 웹서버는 200이라는 약속된 번호를 돌려준다
                 response.end(template); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
-
+            })
+               
         }else{
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-                let title = queryData.id;
-                let template = `
-                <!doctype html>
-                <html>
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            <li><a href="/?id=HTML">HTML</a></li>
-                            <li><a href="/?id=CSS">CSS</a></li>
-                            <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                    </body>
-                </html>
-                `
-                response.writeHead(200);
-                response.end(template); 
+            fs.readdir('./data', function(error, filelist){
+                // 배열의 형태로 출력
+
+                let title = 'Welcome'
+                let description = 'Hello, Node.js'
+                
+                let list = '<ul>';
+                for(i=0; i<filelist.length; i++){
+                    list = list+`<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                }
+                list = list+'</ul>'
+
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                    let title = queryData.id;
+                    let template = `
+                    <!doctype html>
+                    <html>
+                        <head>
+                            <title>WEB1 - ${title}</title>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            ${list}
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                        </body>
+                    </html>
+                    `
+                    response.writeHead(200);
+                    response.end(template); 
+                });
             });
         }
     }else{
