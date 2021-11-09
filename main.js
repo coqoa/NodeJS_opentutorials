f14d92def5f09090854add0fca2643e93e3535a9
coqoa <choibm0208@gmail.com>
2021-11-10 02:15:21 +0900

    211110 Node.js43.App제작-템플릿 기능 정리정돈하기

--- main.js
+++ main.js
@@ -2,35 +2,33 @@ let http = require('http');
 let fs = require('fs');
 let url = require('url'); //url이라는변수로 url모듈을 사용할 것이다.
 let qs = require('querystring');
-const { Z_FIXED } = require('zlib');
 
-function templateHTML(title, list, body, control){
-    return `
-    <!doctype html>
-    <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            ${control}
-            
-            ${body}
-        </body>
-    </html>
-    `;
+let template = {
+    HTML: function(title, list, body, control){
+        return `
+        <!doctype html>
+        <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                ${control}
+                ${body}
+            </body>
+        </html>
+        `;
+    }, list: function(filelist){
+        let list = '<ul>';
+            for(i=0; i<filelist.length; i++){
+                list = list+`<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+            }
+            list = list+'</ul>'
+            return list
+    }
 }
-function templateList(filelist){
-    let list = '<ul>';
-        for(i=0; i<filelist.length; i++){
-            list = list+`<li><a href ="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-        }
-        list = list+'</ul>'
-    return list;
-}
-
 let app = http.createServer(function(request,response){
     let _url = request.url;
     // console.log(_url); // queryString을 알아낼 수 있다.
@@ -39,30 +37,25 @@ let app = http.createServer(function(request,response){
     // console.log(queryData);// http://localhost:3000/?id=HTML 입력시  {id : 'HTML'} 출력 
     //                           -> queryData의 변수에 담긴 값들은 객체.
     // console.log(queryData.id);// HTML 출력
-    
-    
     if(pathname == '/'){
         if(queryData.id == undefined){
-
             fs.readdir('./data', function(error, filelist){// 배열의 형태로 출력
-                let list = templateList(filelist);
                 let title = 'Welcome'
                 let description = 'Hello, Node.js'
-                let template = templateHTML(title, list,
+                let list = template.list(filelist);
+                let html = template.HTML(title, list,
                     `<h2>${title}</h2>${description}`,
                     `<a href="/create">create</a>`);
                 
                 response.writeHead(200); //파일을 성공적으로 전송하면 웹서버는 200이라는 약속된 번호를 돌려준다
-                response.end(template); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
+                response.end(html); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
             })
-            
         }else{
             fs.readdir('./data', function(error, filelist){// 배열의 형태로 출력  
-                let list = templateList(filelist);
-
+                let list = template.list(filelist);
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                     let title = queryData.id;
-                    let template = templateHTML(title, list,
+                    let html = template.HTML(title, list,
                         `<h2>${title}</h2>${description}`,
                         `<a href="/create">create</a>
                         <a href="/update?id=${title}">update</a>
@@ -70,19 +63,18 @@ let app = http.createServer(function(request,response){
                             <input type="hidden" name="id" value="${title}">
                             <input type="submit" value="delete">
                         </form>`
-                        
                     );
                     
                     response.writeHead(200);
-                    response.end(template); 
+                    response.end(html); 
                 });
             });
         }
     }else if(pathname == '/create'){
         fs.readdir('./data', function(error, filelist){// 배열의 형태로 출력
-            let list = templateList(filelist);
+            let list = template.list(filelist);
             let title = 'WEB - create'
-            let template = templateHTML(title, list, `
+            let html = template.HTML(title, list, `
             <form action="/create_process" method="post">
                 <input type="text" name="title" placeholder="title"><br>
                 <textarea name="description" placeholder="description"></textarea><br>
@@ -91,7 +83,7 @@ let app = http.createServer(function(request,response){
             `,'');
             
             response.writeHead(200); //파일을 성공적으로 전송하면 웹서버는 200이라는 약속된 번호를 돌려준다
-            response.end(template); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
+            response.end(html); //주소입력값(쿼리스트링)의 id값을 화면에 출력함
         })
     }else if(pathname == '/create_process'){
         let body = '';
@@ -109,11 +101,11 @@ let app = http.createServer(function(request,response){
         });
     }else if(pathname == '/update'){
         fs.readdir('./data', function(error, filelist){// 배열의 형태로 출력  
-            let list = templateList(filelist);
+            let list = template.list(filelist);
 
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 let title = queryData.id;
-                let template = templateHTML(title, list,
+                let html = template.HTML(title, list,
                     `
                     <form action="/update_process" method="post">
                         <input type="hidden" name="id" value="${title}">
@@ -122,10 +114,10 @@ let app = http.createServer(function(request,response){
                         <input type="submit"><br>
                     </form>
                     `,
-                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
-                
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                );
                 response.writeHead(200);
-                response.end(template); 
+                response.end(html); 
             });
         });
     }else if(pathname == '/update_process'){
@@ -138,7 +130,6 @@ let app = http.createServer(function(request,response){
             let id = post.id;
             let title = post.title;
             let description = post.description;
-            // console.log(post);
             fs.rename(`data/${id}`,`data/${title}`, function(error){
                 fs.writeFile(`data/${title}`, description, 'utf8', function(err){
                     response.writeHead(302, {Location: `/?id=${title}`});
@@ -146,7 +137,6 @@ let app = http.createServer(function(request,response){
                 })
             })
         });
-    
     }else if(pathname == '/delete_process'){
         let body = '';
         request.on('data', function(data){
@@ -160,12 +150,7 @@ let app = http.createServer(function(request,response){
                 response.end();
             })
         });
-    
-    
-    
-    
-    
-    
+
     }else{
         response.writeHead(404); //파일을 찾을 수 없으면 웹서버는 404라는 약속된 번호를 돌려준다
         response.end('Not found');
